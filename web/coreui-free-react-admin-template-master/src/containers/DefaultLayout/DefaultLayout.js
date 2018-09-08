307a04997cca163356e6b62e6d03257bd2ff25cd
acrdlph <achill@way.network>
2018-09-08 12:02:49 +0200

    template added actually

--- web/coreui-free-react-admin-template-master/src/containers/DefaultLayout/DefaultLayout.js
+++ web/coreui-free-react-admin-template-master/src/containers/DefaultLayout/DefaultLayout.js
@@ -30,13 +30,7 @@ class DefaultLayout extends Component {
           <DefaultHeader />
         </AppHeader>
         <div className="app-body">
-          <AppSidebar fixed display="lg">
-            <AppSidebarHeader />
-            <AppSidebarForm />
-            <AppSidebarNav navConfig={navigation} {...this.props} />
-            <AppSidebarFooter />
-            <AppSidebarMinimizer />
-          </AppSidebar>
+          
           <main className="main">
             <AppBreadcrumb appRoutes={routes}/>
             <Container fluid>
